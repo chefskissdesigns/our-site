6f4e39f3d198fc89179fddeafb43362e2b7c9d31
chefskissdesigns <109132807+chefskissdesigns@users.noreply.github.com>
2023-06-22 12:17:19 -0500

    js func introduced for footer year

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function getCopyYearElement() {
+    const copyYearElement = document.getElementById('copy-year');
+    return copyYearElement;
+}
+
+function setCopyYearElement(element, yearString) {
+    element.innerText = yearString;
+}
+
+function getCurrentYear() {
+    const curYear = new Date().getFullYear.toString();
+    return curYear;
+}
+
+function main() {
+    setCopyYearElement(getCopyYearElement(), getCurrentYear());
+}
+
+main();
